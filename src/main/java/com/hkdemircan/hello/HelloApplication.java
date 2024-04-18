d2dd319a0c3029d0607e5b5356666c63a580fef7
jganthakoru <94169425+jganthakoru@users.noreply.github.com>
2024-04-18 20:34:02 +0530

    Update HelloApplication.java

--- src/main/java/com/hkdemircan/hello/HelloApplication.java
+++ src/main/java/com/hkdemircan/hello/HelloApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @GetMapping("/display")
+    public String getMessage(){
+        return "Hello World with Docker";
     }
 
 }
