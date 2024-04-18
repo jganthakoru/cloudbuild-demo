fefcbcf1032aa33034a5dcbf34d49c6cab42a8ec
jganthakoru <94169425+jganthakoru@users.noreply.github.com>
2024-04-18 20:34:27 +0530

    Update HelloWorld.java

--- src/main/java/com/hkdemircan/hello/HelloWorld.java
+++ src/main/java/com/hkdemircan/hello/HelloWorld.java
@@ -3,10 +3,10 @@ package com.hkdemircan.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class HelloWorld {
 
-    @GetMapping("/")
+    
     public String getMessage(){
         return "Hello World with Docker";
     }
